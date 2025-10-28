7d16704616949a5390a7e2fe15bc42a173414ee7
xiaoyu_jie <kclon29@gmail.com>
2025-10-28 16:05:44 +0800

    Delete SteamAccountManager directory

--- SteamAccountManager/Models/Settings.cs
+++
@@ -1,6 +0,0 @@
-﻿// Models/Settings.cs
-public class Settings
-{
-    public string SteamApiKey { get; set; }
-    public string SteamExePath { get; set; } = @"C:\Program Files (x86)\Steam\steam.exe"; // 默认值
-}
